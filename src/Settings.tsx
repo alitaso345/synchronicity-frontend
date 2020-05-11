16884ab989768c21f40b9504a5aba1533e0d3936
alitaso345 <alice.maru345@gmail.com>
2020-05-11 23:06:26 +0900

    add setting form

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,8 +1,58 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { putSettingsPath } from './paths'
 
 const Setting: React.FC = () => {
+  const [hashTag, updateHashTag] = useState('')
+  const [channel, updateChannel] = useState('')
+
+  useEffect(
+    () => {
+      updateHashTag('#mogra')
+      updateChannel('#mogra')
+    },
+    []
+  )
+
+  const submitHandler = async (): Promise<void> => {
+    const res = await fetch(putSettingsPath, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify({ hashTag: hashTag, channel: channel})
+    })
+
+    if (res.status === 204) {
+      alert('設定変更が完了しました')
+    } else {
+      alert('設定の変更に失敗しました')
+    }
+  }
+
   return (
-    <div>setting page</div>
+    <div>
+      <h2>設定ページ</h2>
+      <div>
+        <label>Twitterハッシュタグ</label>
+        <input
+          placeholder="#mogra"
+          value={hashTag}
+          onChange={(e) => updateHashTag(e.currentTarget.value)}
+        />
+      </div>
+
+      <div>
+        <label>Twitchチャンネル名</label>
+        <input
+          placeholder="#mogra"
+          value={channel}
+          onChange={(e) => updateChannel(e.currentTarget.value)}
+        />
+      </div>
+
+      <button onClick={submitHandler}>変更する</button>
+    </div>
   )
 }
 
