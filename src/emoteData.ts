582dd3eb9383480d7c1163befe4f671f04d009e1
alitaso345 <alice.maru345@gmail.com>
2020-05-15 22:59:58 +0900

    add emote

---
+++ src/emoteData.ts
@@ -0,0 +1,135 @@
+interface Emote {
+  name: string
+  url: string
+  width: number
+  height: number
+}
+
+export const emoteData: Emote[] = [
+  {
+    "name": "mograHo",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/193202/1.0"
+  },
+  {
+    "name": "mograA",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/193203/1.0"
+  },
+  {
+    "name": "mograB",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/193206/1.0"
+  },
+  {
+    "name": "mograMelo",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/193208/1.0"
+  },
+  {
+    "name": "mograDyama",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/258933/1.0"
+  },
+  {
+    "name": "mograKei",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/258957/1.0"
+  },
+  {
+    "name": "mograMelo2",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/414662/1.0"
+  },
+  {
+    "name": "mograMadyama",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/419405/1.0"
+  },
+  {
+    "name": "mograOblongar",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/787597/1.0"
+  },
+  {
+    "name": "mograEva",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/787614/1.0"
+  },
+  {
+    "name": "mograpTechCat",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/1293794/1.0"
+  },
+  {
+    "name": "mograYega",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/1354366/1.0"
+  },
+  {
+    "name": "mograYako3",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/1402941/1.0"
+  },
+  {
+    "name": "mograInotaku",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/1660570/1.0"
+  },
+  {
+    "name": "mograWildP",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/1830516/1.0"
+  },
+  {
+    "name": "mograInu",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/300572362/1.0"
+  },
+  {
+    "name": "mograKz",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/301587393/1.0"
+  },
+  {
+    "name": "mograWildp2",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/302051656/1.0"
+  },
+  {
+    "name": "mograSo",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/302051665/1.0"
+  },
+  {
+    "name": "mograMitsu",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/302051667/1.0"
+  },
+  {
+    "name": "mograWildPBig",
+    "height": 28,
+    "width": 28,
+    "url": "https://static-cdn.jtvnw.net/emoticons/v1/302051767/1.0"
+  }
+]
